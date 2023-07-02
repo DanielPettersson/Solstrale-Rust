f03b65eb90025202e16ba323c321e17ed1a3625c
Daniel Pettersson <daniel.g.pettersson@gmail.com>
2023-07-02 13:34:05 +0200

    Added doc test to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,54 @@
 //! * Multi-threaded Bvh creation to greatly speed up rendering
 //! * Post processing of rendered images using [Open Image Denoise](https://www.openimagedenoise.org/)
 //!
+//! ## Example:
+//! ```rust
+//! # use std::sync::mpsc::channel;
+//! # use std::thread;
+//! # use image::RgbImage;
+//! # use solstrale::camera::CameraConfig;
+//! # use solstrale::geo::vec3::Vec3;
+//! # use solstrale::hittable::{HittableList, Sphere, Hittable};
+//! # use solstrale::material::{DiffuseLight, Lambertian};
+//! # use solstrale::material::texture::SolidColor;
+//! # use solstrale::ray_trace;
+//! # use solstrale::renderer::{RenderConfig, Scene};
+//! # use solstrale::renderer::shader::PathTracingShader;
+//! let render_config = RenderConfig {
+//!     samples_per_pixel: 50,
+//!     shader: PathTracingShader::new(50),
+//!     post_processor: None,
+//! };
+//! let camera = CameraConfig {
+//!     vertical_fov_degrees: 20.,
+//!     aperture_size: 0.1,
+//!     look_from: Vec3::new(0., 0., 4.),
+//!     look_at: Vec3::new(0., 0., 0.),
+//! };
+//! let mut world = HittableList::new();
+//! let yellow = Lambertian::new(SolidColor::new(1., 1., 0.), None);
+//! let light = DiffuseLight::new(10., 10., 10., None);
+//! world.add(Sphere::new(Vec3::new(0., 0., 0.), 0.5, yellow));
+//!
+//! let scene = Scene {
+//!     world,
+//!     camera,
+//!     background_color: Vec3::new(0.2, 0.3, 0.5),
+//!     render_config,
+//! };
+//!
+//! let (output_sender, output_receiver) = channel();
+//! let (_, abort_receiver) = channel();
+//!
+//! thread::spawn(move || {
+//!     ray_trace(800, 400, scene, &output_sender, &abort_receiver).unwrap();
+//! });
+//!
+//! for render_output in output_receiver {
+//!     let _image = render_output.render_image;
+//! }
+//! ```
+//!
 //! ## Example output
 //! ![sponza-bump2](https://github.com/DanielPettersson/solstrale-rust/assets/3603911/0ab79ed9-cddf-46b1-84e7-03cef35f5600)
 //!
