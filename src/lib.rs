2739869228a6653f30f825ee739a6b95674d886d
Daniel Pettersson <daniel.g.pettersson@gmail.com>
2023-07-16 16:53:14 +0200

    Updated docs

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,8 @@
 //! * Loading of obj models with included materials
 //! * Multi-threaded Bvh creation to greatly speed up rendering
 //! * Post processing of rendered images using [Open Image Denoise](https://www.openimagedenoise.org/)
+//! * Bump mapping
+//! * Light attenuation
 //!
 //! ## Example:
 //! ```rust
@@ -60,8 +62,9 @@
 //! ```
 //!
 //! ## Example output
+//! ![happy](https://github.com/DanielPettersson/solstrale-rust/assets/3603911/c5357792-a3dc-42f9-8230-320140f9c30e)
 //! ![sponza-bump2](https://github.com/DanielPettersson/solstrale-rust/assets/3603911/0ab79ed9-cddf-46b1-84e7-03cef35f5600)
-//!
+
 //! ## Credits
 //! The ray tracing is inspired by the excellent [Ray Tracing in One Weekend Book Series](https://github.com/RayTracing/raytracing.github.io) by Peter Shirley
 
